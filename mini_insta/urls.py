45f8760cfad8d5ffb0966b0ea1feb93d7cb544e4
lr0826 <lr0826@bu.edu>
2025-09-25 12:09:02 -0400

    finished assignment 3

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,8 +3,9 @@
 # Description: The url python file for the mini-insta application
 from django.urls import path
 from django.conf import settings
-from .views import ProfileListView
+from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
     path('', ProfileListView.as_view(), name="show_all_profiles"),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -2,7 +2,7 @@
 # Author: Run Liu (lr0826@bu.edu), 9/23/2025
 # Description: The view python file for the mini-insta application
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 # Create your views here.
 
@@ -14,3 +14,11 @@ class ProfileListView(ListView):
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    ''' obtain data for one Profile record, and to delegate work to 
+    a template called show_profile.html to display that Profile.'''
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile"
+    
